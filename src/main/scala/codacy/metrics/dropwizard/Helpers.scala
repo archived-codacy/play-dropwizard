c407e1686b46addfdf6c2fbf77ee953366870ac4
Johann Egger <johann@codacy.com>
2016-01-21 09:32:34 +0200

    Avoid leaking timer context when block throws exception

--- src/main/scala/codacy/metrics/dropwizard/Helpers.scala
+++ src/main/scala/codacy/metrics/dropwizard/Helpers.scala
@@ -1,14 +1,18 @@
 package codacy.metrics.dropwizard
 
 import scala.concurrent.Future
+import scala.util.{Failure, Success, Try}
 
 private[dropwizard] trait Helpers{
 
   def timed[A](timerName: TimerName)(block: => A): A = {
     val ctx = MetricRegistry.timer(timerName).map(_.time())
-    val res = block
+    val res = Try(block)
     ctx.foreach(_.stop())
-    res
+    res match{
+      case Success(result) => result
+      case Failure(exception) => throw exception
+    }
   }
 
   def timedAsync[A](timerName: TimerName)(block: => Future[A]): Future[A] = {
